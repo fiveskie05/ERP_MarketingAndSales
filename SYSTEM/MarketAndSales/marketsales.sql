4fe462698f7a9ae6852ff5dc845c2c96dbdbcd87
fiveskie05 <earondero@gmail.com>
2018-12-04 15:04:29 +0800

    database update Rondero

--- SYSTEM/MarketAndSales/marketsales.sql
+++ SYSTEM/MarketAndSales/marketsales.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 04, 2018 at 07:28 AM
+-- Generation Time: Dec 04, 2018 at 08:03 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -41,10 +41,10 @@ CREATE TABLE `products_and_services` (
 --
 
 INSERT INTO `products_and_services` (`ID`, `Product_code`, `Services`, `Amount`, `No_of_person`) VALUES
-(25, 'Wine 001', 'Lambanog', 500, 10),
-(26, 'Wine 002', 'Banayad Whiskey', 850, 10),
-(27, 'Wine 003', 'Lambangag', 700, 10),
-(28, 'Wine 004', 'Black Horse', 900, 10);
+(25, 'Wine 001', 'Lambanog', 500, 1),
+(26, 'Wine 002', 'Banayad Whiskey', 850, 1),
+(27, 'Wine 003', 'Lambangag', 700, 1),
+(28, 'Wine 004', 'Black Horse', 900, 1);
 
 -- --------------------------------------------------------
 
@@ -67,6 +67,20 @@ CREATE TABLE `recenttransaction` (
   `Product_code` varchar(255) NOT NULL,
   `Sub_total` smallint(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recenttransaction`
+--
+
+INSERT INTO `recenttransaction` (`id`, `r_date`, `First_name`, `Last_name`, `Address`, `Gender`, `Age`, `Contact_Number`, `No_of_person`, `Product_name`, `Product_price`, `Product_code`, `Sub_total`) VALUES
+(63, '2018-12-04', 'qwed', '123', 'wawa', 'Female', 21, 32767, 10, 'Lambanog', 500, 'Wine 001', 5000),
+(64, '2018-12-04', 'qwed', '123', 'wawa', 'Female', 21, 32767, 10, 'Banayad Whiskey', 850, 'Wine 002', 8500),
+(65, '2018-12-04', 'qwed', '123', 'wawa', 'Female', 21, 32767, 10, 'Lambangag', 700, 'Wine 003', 7000),
+(66, '2018-12-04', 'qwed', '123', 'wawa', 'Female', 21, 32767, 10, 'Black Horse', 900, 'Wine 004', 9000),
+(67, '2018-12-04', 'Paul', 'PAPAPa', 'zxxccxcxcx', 'Female', 18, 32767, 1, 'Lambanog', 500, 'Wine 001', 500),
+(68, '2018-12-04', 'Paul', 'PAPAPa', 'zxxccxcxcx', 'Female', 18, 32767, 1, 'Banayad Whiskey', 850, 'Wine 002', 850),
+(69, '2018-12-04', 'Paul', 'PAPAPa', 'zxxccxcxcx', 'Female', 18, 32767, 1, 'Lambangag', 700, 'Wine 003', 700),
+(70, '2018-12-04', 'Paul', 'PAPAPa', 'zxxccxcxcx', 'Female', 18, 32767, 1, 'Black Horse', 900, 'Wine 004', 900);
 
 -- --------------------------------------------------------
 
@@ -98,6 +112,17 @@ CREATE TABLE `time_log` (
   `Time_in` varchar(255) NOT NULL,
   `Time_out` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `time_log`
+--
+
+INSERT INTO `time_log` (`t_ID`, `U_name`, `Position`, `Date_loggedin`, `Time_in`, `Time_out`) VALUES
+(130, 'Jamaica Tesbun', 'Admin', '2018-12-04', '02:57:20pm', ''),
+(131, 'Niela Pipi', 'Staff', '2018-12-04', '02:57:38pm', ''),
+(132, 'Jamaica Tesbun', 'Admin', '2018-12-04', '02:58:59pm', ''),
+(133, 'Niela Pipi', 'Staff', '2018-12-04', '03:00:16pm', ''),
+(134, 'Jamaica Tesbun', 'Admin', '2018-12-04', '03:01:04pm', '');
 
 -- --------------------------------------------------------
 
@@ -174,19 +199,19 @@ ALTER TABLE `products_and_services`
 -- AUTO_INCREMENT for table `recenttransaction`
 --
 ALTER TABLE `recenttransaction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `temp_table`
 --
 ALTER TABLE `temp_table`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `time_log`
 --
 ALTER TABLE `time_log`
-  MODIFY `t_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `t_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
 
 --
 -- AUTO_INCREMENT for table `users`
